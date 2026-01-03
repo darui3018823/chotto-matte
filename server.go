77c8a886aeec2815c2426d30ab9b05d22f217ceb
darui3018823 <151906210+darui3018823@users.noreply.github.com>
2026-01-04 02:44:47 +0900

    Fix: Set correct MIME types for JS and CSS in Go server

--- server.go
+++ server.go
@@ -6,11 +6,29 @@ import (
 )
 
 func main() {
-	fs := http.FileServer(http.Dir("."))
-	http.Handle("/", fs)
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/js/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/javascript; charset=utf-8")
+		http.ServeFile(w, r, "."+r.URL.Path)
+	})
+	mux.HandleFunc("/css/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/css; charset=utf-8")
+		http.ServeFile(w, r, "."+r.URL.Path)
+	})
+	mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
+
+	mux.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
+		http.NotFound(w, r)
+	})
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		http.ServeFile(w, r, "index.html")
+	})
 
 	log.Println("Serving at http://localhost:8080...")
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
 		log.Fatal(err)
 	}
